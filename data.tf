58337c5f41936297944e88cad13e058b683218e4
ravisinghrajput95 <92205137+ravisinghrajput95@users.noreply.github.com>
2021-10-23 23:54:25 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
 data "google_client_config" "default" {}
 
-data "google_container_cluster" "cluuster" {
+data "google_container_cluster" "cluster" {
   name = module.gke-cluster.name
   location = var.region
 }
