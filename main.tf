187601ce9cf49cb310c83f1e9885716e9f401c7e
ravisinghrajput95 <92205137+ravisinghrajput95@users.noreply.github.com>
2021-10-23 23:56:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -63,7 +63,20 @@ provider "google-beta" {
 # We use this data provider to expose an access token for communicating with the GKE cluster.
 data "google_client_config" "client" {}
 
+data "google_container_cluster" "cluster" {
+  name = module.gke-cluster.name
+  location = var.region
+}
+  
+provider "kubernetes" {
+  load_config_file = false
+  host = google_container_cluster.cluster.endpoint
+  token = data.google_client_config.default.access_token
 
+  client_certificate = base64decode(google_container_cluster.cluster.master_auth[0].client_certificate)
+  client_key = base64decode(google_container_cluster.cluster.master_auth[0].client_key)
+  cluster_ca_certificate = base64decode(google_container_cluster.cluster.master_auth[0].cluster_ca_certificate)
+}
 
 # ---------------------------------------------------------------------------------------------------------------------
 # DEPLOY A PUBLIC CLUSTER IN GOOGLE CLOUD PLATFORM
